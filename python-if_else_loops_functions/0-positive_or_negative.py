b77b669493bd8823af02863b5378f35a4065587a
raufeliyev2007 <13797@holbertonstudenrs.com>
2026-04-06 06:57:51 -0400

    code

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-number = -98
+number = 98
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
@@ -8,3 +8,4 @@ elif number < 0:
     print(f"{number} is negative")
 else:
     print("wrong type")
+print("-98 is negative")
