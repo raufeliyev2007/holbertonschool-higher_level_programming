0ece7e99abb67667105565586316fdbea90c5d19
raufeliyev2007 <13797@holbertonstudenrs.com>
2026-04-06 12:34:37 -0400

    code

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    """Prints the last digit of a number and returns it."""
-    last_digit = abs(number) % 10
-    print(last_digit, end="")
-    return last_digit
+def add(a,b):
+    return a+b
