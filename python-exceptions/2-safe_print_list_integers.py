f78dffa89064f03584018446d1b7330e27c7a36b
raufeliyev2007 <13797@holbertonstudenrs.com>
2026-04-14 10:03:18 -0400

    Code

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,12 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
+            # Если i >= len(my_list), здесь упадет IndexError
+            # Это именно то, что нужно по условию
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
+            # Пропускаем только ошибки типа (не целое число)
             continue
-        except IndexError:
-            # Если x больше длины списка, мы не должны упасть
-            break
     print("")
     return count
